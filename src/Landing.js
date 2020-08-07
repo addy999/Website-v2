11e5ddfd1bd2aa038663bb975ad7a108afa3e75f
Addy Bhatia <addy@loftorbital.com>
2020-08-06 22:58:59 -0400

    space easter egg

--- src/Landing.js
+++ src/Landing.js
@@ -6,60 +6,78 @@ import linkedin from './assests/linkedin.gif';
 import email from './assests/email.png';
 
 import './css/Landing.css';
-import './css/stars.css';
+import './css/stars2.css';
 
-function Landing({stars}) {
+class Landing extends React.Component {
 
-  const stars_visible = {
-    "display" : stars ? "block" : "None",
-    "opacity" : 0.5,
-  }
+    constructor(props) {
+      super(props);
+      this.state = {
+        mouseover : false,
+      }
+    }
 
-  return (
+    stars_visible = () => {
+      return {
+        "opacity" : this.state.mouseover ? 1 : 0,
+        "transition" : "all 0.5s",
+      }
+    }
 
-    <div className="Landing">
+    render() {
+      return (
+        <div className="Landing">
 
-      <div id='stars' style={stars_visible}></div>
-      <div id='stars2' style={stars_visible}></div>
-      <div id='stars3' style={stars_visible}></div>
+        {/* Scrolling stars   */}
 
-      <header className="header row">
+        {/* <div id='stars'></div>
+        <div id='stars2'></div>
+        <div id='stars3'></div> */}
 
-        <div className="col">
-          <Dp />
-        </div>
-        <div id="desc" className="col">
-          <h1 className="display-4">
-            Addy <span className="blue">Bhatia</span>
-          </h1>
-          <br></br>
-          <p className="desc-text">Nice to see you, it's been a while.</p>
-          <p className="desc-text bold blue">Full-stack and Software Dev.</p>
-          <p className="desc-text">I code everything from space tech to ML pipelines.</p>
-          <p className="desc-text">Explore my projects and give a shout.</p>
+        {/* Twinkling stars   */}
 
-          <br></br>
+        <div className="stars" style={this.stars_visible()}></div>
+        <div className="twinkling" style={this.stars_visible()}></div>
 
-          <div className="row butt">
+        <header className="header row">
 
-            <button type="button" className="btn btn-outline-dark col-sm">
-              <img className="contact-img" src={github}></img>
-            </button>
-
-            <button type="button" className="btn btn-outline-primary col-sm">
-              <img className="contact-img" src={linkedin}></img>
-            </button>
-
-            <button type="button" className="btn btn-outline-info col-sm">
-            <img className="contact-img" src={email}></img>
-            </button>
-
+          <div className="col">
+            <Dp />
           </div>
-        </div>
+          
+          <div id="desc" className="col">
+            <h1 className="display-4">
+              Addy <span className="blue">Bhatia</span>
+            </h1>
+            <br></br>
+            <p className="desc-text">Nice to see you, it's been a while.</p>
+            <p className="desc-text bold blue">Full-stack and Software Dev.</p>
+            <p className="desc-text">I code everything from <span className={this.state.mouseover ? "blue" : ""} onMouseEnter={() => this.setState({mouseover:true})} onMouseLeave={() => this.setState({mouseover:false})}>space tech</span> to ML pipelines.</p>
+            <p className="desc-text">Explore my projects and give a shout.</p>
 
-      </header>
-    </div>
-  );
+            <br></br>
+
+            <div className="row butt">
+
+              <button type="button" className="btn btn-outline-dark col-sm">
+                <img className="contact-img" src={github}></img>
+              </button>
+
+              <button type="button" className="btn btn-outline-primary col-sm">
+                <img className="contact-img" src={linkedin}></img>
+              </button>
+
+              <button type="button" className="btn btn-outline-info col-sm">
+              <img className="contact-img" src={email}></img>
+              </button>
+
+            </div>
+          </div>
+
+        </header>
+      </div>
+      );
+    }
 }
 
 export default Landing;
