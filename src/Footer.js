43e718e9a5ebad2aa90de1b679a8a6b4c325a1b1
addy999 <jude.addy999@gmail.com>
2020-08-10 13:56:10 -0400

    add tech stack in footer

--- src/Footer.js
+++ src/Footer.js
@@ -20,7 +20,7 @@ class Footer extends React.Component {
 
         return (
             <div className="Footer text-center" style={style}>
-                <p>Developed with <svg className="bi bi-heart-fill" style={{"color":"#ff7089"}} width="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" clipRule="evenodd"/></svg> in Toronto.</p>
+                <p>Developed with <svg className="bi bi-heart-fill" style={{"color":"#ff7089"}} width="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" clipRule="evenodd"/></svg> in Toronto. <br></br>(and React + Node.JS)</p>
             </div>
         );
     }
