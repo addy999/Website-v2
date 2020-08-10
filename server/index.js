8ffb992fec30d74c3c2d4ad2796a2977aadb7199
addy999 <jude.addy999@gmail.com>
2020-08-09 23:33:44 -0400

    prod server

--- server/index.js
+++ server/index.js
@@ -40,6 +40,16 @@ app.get('/api/downVote', (req, res) => {
   res.send(JSON.stringify({ score: `${score}` }));
 })
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, '../build')));
+    
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+  });
+}
+
 app.listen(3001, () =>
   console.log('Express server is running on localhost:3001')
 );
