e038f5b4fd0048011a957a29d260bff74c2544a3
addy999 <jude.addy999@gmail.com>
2020-08-10 23:38:38 -0400

    style comment section

--- src/Comments.js
+++ src/Comments.js
@@ -6,8 +6,15 @@ class Comments extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-          id : props.id
+          id : props.id,
+          submitMouse : false
       }
+      this.ref = React.createRef();
+      this.submit_btn = React.createRef();
+    }
+
+    close = () => {
+        this.ref.current.style.display="none";
     }
 
     sortComments = (cmnts) => {
@@ -39,7 +46,6 @@ class Comments extends React.Component {
     render() {
 
         var cmnts = this.loadComments();
-        console.log(cmnts);
 
         const style = {
             "position" : "relative",
@@ -48,10 +54,10 @@ class Comments extends React.Component {
             "transform" : "translate(-50%,50%)",
             "width" : "75vh",
             "height" : "50vh",
-            "boxShadow" : "10px 10px 5px 0px rgba(0,0,0,0.75)",
-            "border" : "#96FFF2",
-            "borderStyle" : "solid",
-            "display" : "table"
+            "boxShadow" : "4px 4px 12px 0px rgba(0, 0, 0, 0.75)",
+            // "border" : "#96FFF2",
+            // "borderStyle" : "solid",
+            "display" : this.props.display ? "table" : "none",
         }
 
         const title_style = {
@@ -66,20 +72,46 @@ class Comments extends React.Component {
             'position': 'absolute',
             'left': 0,
             'top': '25%',
-            'bottom': "15%",
+            'bottom': "10vh",
             'margin': 0,
             "padding" : "2vw",
             "overflow": "auto"
         }
+        const input_style = {
+            position : "absolute",
+            left : "2vw",
+            bottom : "2vh",
+            margin : 0,
+            right:"2vw"
+        }
 
         return (
-            <div style={style}>
+            <div style={style} ref={this.ref}>
                 <div style={title_style}>
-                    <p className="display-4" style={{"font-size":"inherit"}}>Leave feedback</p>
+                    <p className="display-4" style={{"font-size":"inherit", alignContent:"center"}}>Leave feedback 
+                    <span 
+                    style={{float:"right", transform: 'translateY(35%)', pointer : "pointer!important"}} 
+                    className="material-icons"
+                    onClick={this.close}
+                    >close</span>
+                    </p>                    
                     <hr></hr>
                 </div>
                 <div style={feed_style}>
                     {Object.entries(cmnts).map((t) =>  <Comment time={t[0]} comment={t[1]} />)}
+                </div>
+                <div className="row form-group" style={input_style}>
+                    <input 
+                    className="col form-control" 
+                    type="text"
+                    placeholder="Leave feedback for this project"
+                    style={{margin: 'auto', marginRight : "20px", height : "45px" }}></input>
+                    <button className="col btn btn-info"  ref={this.submit_btn}
+                    onMouseEnter={()=>this.setState({submitMouse : true})}
+                    onMouseLeave={()=>this.setState({submitMouse : false})}
+                    style={{maxWidth : "fit-content", padding:"10px", margin: 'auto', height : "45px", backgroundColor : this.state.submitMouse ? "#96FFF2" : "#5d9e96"}}>
+                        Submit
+                    </button>
                 </div>
             </div>
         );
